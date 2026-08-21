2e9dab6793e1c1c7079a4c58b04fd8c90c3cfb68
Mateus Buchalla Santos <mbuchalla23@gmail.com>
2026-08-21 18:30:02 -0300

    Update codigo.ino
    Adicionando funções mais fáceis de ler; adicionando ponte H 095 como interruptor de motores

--- Robotica/codigo.ino
+++ Robotica/codigo.ino
@@ -1,44 +1,73 @@
-#define motorPin 9
-#define triggerPin 7
+// Pinos do Sensor Ultrassônico
+#define triggerPin 3
 #define echoPin 4
 
+// Pinos do Driver de Motor L298N/L293D (conforme imagens)
+
+#define IN1 6
+#define IN2 9  
+#define IN3 10
+#define IN4 11
 
 
-
-long readUltrasonicDistance(int trigger, int echo)
-{
-  pinMode(trigger, OUTPUT);  // Clear the trigger;
-  digitalWrite(trigger, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds;
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
-  pinMode(echo, INPUT);
-   // Reads the echo pin, and returns the sound wave travel time in microseconds;
-  return pulseIn(echo, HIGH);
+// Função para medir a distância via ultrassom
+long readUltrasonicDistance(int trigger, int echo) {
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+    
+    return pulseIn(echo, HIGH);
 }
 
-void setup(){
- pinMode(triggerPin, INPUT);
- pinMode(echoPin, INPUT);
- pinMode(motorPin, OUTPUT);
+void ligarMotores(){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
 };
 
+void desligarMotores(){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
 
+void setup() {
+    // Configuração dos pinos do sensor
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 
-void loop()
-{
-   /*
-    0.01723 = constant to transform milimeters (mm) value to centimeters (cm);
-    
-    
-    If the distance is lower than 30 cm, turns on the motor*/
-  if(0.01723*readUltrasonicDistance(triggerPin, echoPin) < 30){
-    digitalWrite(motorPin, HIGH);
-  } 
-    // If isn't, keeps off.
-  else {
-    digitalWrite(motorPin, LOW);
-  }
+    // Configuração dos pinos do motor
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    // Estado inicial: motor desligado
+    desligarMotores();
+
+    Serial.begin(9600);
+}
+
+void loop() {
+    // Cálculo da distância em centímetros
+    float distancia = 0.01723 * readUltrasonicDistance(triggerPin, echoPin);
+
+    // Condição: Ativa o motor se houver presença até 30 cm
+    if (distancia < 30.0 && distancia > 0.0) {
+        ligarMotores();
+    } else {
+        desligarMotores();
+    }
+
+    // Monitoramento via Serial
+    Serial.print("Distância: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
+
+    delay(50); // Leve pausa para estabilizar a leitura
 }
